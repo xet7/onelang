78e466393e2b373e77974791a9bba0b036deb12a
Lauri Ojansivu <x@xet7.org>
2018-05-05 23:01:20 +0300

    Add install-build-dependencies.sh script to clone repos.

---
+++ install-build-dependencies.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm -rf CompilerBackend
+git clone git@github.com:xet7/onelang_CompilerBackend CompilerBackend
+
+rm -rf generated
+git clone git@github.com:xet7/onelang_generated generated
