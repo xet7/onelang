7d6b446f6881e6506e772748baab13cc074c437c
Lauri Ojansivu <x@xet7.org>
2019-09-09 07:16:56 +0300

    Update dependencies.

--- install-build-dependencies.sh
+++ install-build-dependencies.sh
@@ -6,4 +6,4 @@ git clone git@github.com:xet7/onelang_CompilerBackend CompilerBackend
 rm -rf generated
 git clone git@github.com:xet7/onelang_generated generated
 
-sudo apt-get install maven mono-complete
+sudo apt-get install -y maven mono-complete
